c2f708f6d692cff58799ee639735b4c0bcced1be
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-12-26 09:28:25 +0100

    added cors

---
+++ CalculPolynome-backend/service-factorisation/src/main/java/com/emsi/servicefactorisation/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.emsi.servicefactorisation.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // Allow specific origin
+        config.addAllowedMethod("*"); // Allow all HTTP methods
+        config.addAllowedHeader("*"); // Allow all headers
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/", config);
+        return new CorsFilter(source);
+    }
+}
